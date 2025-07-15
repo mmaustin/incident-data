a76bdcaee6f8f136cac8831d2f4ed494085c1bde
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-15 14:42:49 -0400

    Built an async function to fetch and stringify shooting incident data

---
+++ incident/utils/actions.ts
@@ -0,0 +1,19 @@
+'use server';
+
+
+export const incidentData = async(): Promise<string> => {
+  try {
+    const dataPromise = await fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json");
+    const dataUnwrapped = await dataPromise.json();
+
+    const dataToString = JSON.stringify(dataUnwrapped);
+
+    return dataToString;
+    
+  } catch (error) {
+    console.log(error);    
+  };
+
+  return "There was an error while fetching the data.";
+
+}
