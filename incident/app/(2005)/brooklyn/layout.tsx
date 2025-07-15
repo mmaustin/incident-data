36b5e8213ede06894afa2ca1cad646c01fb47825
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-15 16:21:35 -0400

    Added a layout file to the brooklyn folder

---
+++ incident/app/(2005)/brooklyn/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import DataContextProvider from "@/providers/dataProvider"
+import { ReactNode } from 'react';
+
+// Can't do it in a secondary layout either
+const BrooklynLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <DataContextProvider>
+      <div className="">
+        {children}
+      </div>
+    </DataContextProvider>
+    
+  )
+}
+export default BrooklynLayout
