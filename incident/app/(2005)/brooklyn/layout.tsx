2cb1f8c6d2020dfbb2f9ba22566d9bf1c9e9f8fc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-16 15:57:11 -0400

    Fetched incident data in brooklyn folder's layout file

--- incident/app/(2005)/brooklyn/layout.tsx
+++ incident/app/(2005)/brooklyn/layout.tsx
@@ -1,13 +1,20 @@
-'use client';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 
 //import DataContextProvider from "@/providers/dataProvider"
+import { incidentData } from '@/utils/actions';
 import { ReactNode } from 'react';
 
 // Can't do it in a secondary layout either
-const BrooklynLayout = ({children}: {children: ReactNode}) => {
+const BrooklynLayout = async ({children}: {children: ReactNode}) => {
+
+  const d = await incidentData();
+  const dd: object[] = JSON.parse(d);
+  //console.log(dd.slice(400,409));
+
   return (
     // <DataContextProvider>
-      <div className="">
+      <div className="bg-amber-700">
         {children}
       </div>
     // {/* </DataContextProvider> */}
--- incident/app/(2005)/brooklyn/page.tsx
+++ incident/app/(2005)/brooklyn/page.tsx
@@ -1,18 +1,18 @@
 //'use client';
 
-import { incidentData } from "@/utils/actions";
+//import { incidentData } from "@/utils/actions";
 //import { use } from "react";
 
 const BrooklynIncidentsPlots = async() => {
 
-  const d = await incidentData();
-  const dd: object[] = JSON.parse(d);
-  console.log(dd.slice(400,409));
+  // const d = await incidentData();
+  // const dd: object[] = JSON.parse(d);
+  // console.log(dd.slice(400,409));
   
   
 
   return (
-    <div>BrooklynIncidentsPlots</div>
+    <div className="h-screen">BrooklynIncidentsPlots</div>
   )
 }
 export default BrooklynIncidentsPlots;
