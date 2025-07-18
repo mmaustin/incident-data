c9ecc7e32350b98ebf1c4b87515999e22a3d0211
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-18 15:33:27 -0400

    Continued troubleshooting of context value not setting

--- incident/components/clientProvider/DataProvider.tsx
+++ incident/components/clientProvider/DataProvider.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { createContext, ReactNode, useContext, use  } from 'react';
+import { createContext, ReactNode, useContext, use, useState  } from 'react';
 
 const DataContext = createContext("Awaiting Stringified Data");
 
 const DataProvider = ({children, promiseValue}: {children: ReactNode, promiseValue: Promise<string> }) => {
   const propData = use(promiseValue);
 
+  const [constantData, ] = useState(propData)
+  
+//console.log(Object.keys(DataContext['_currentValue']));
+
   return (
-    <DataContext value={propData}>
+    <DataContext value={constantData}>
       {children}
     </DataContext>
   )
