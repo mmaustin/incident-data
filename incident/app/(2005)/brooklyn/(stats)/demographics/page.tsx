b0b97d45fba675080abcb66ea81c5e26d4664dca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-18 15:42:05 -0400

    Commented out all code related to the data provider

--- incident/app/(2005)/brooklyn/(stats)/demographics/page.tsx
+++ incident/app/(2005)/brooklyn/(stats)/demographics/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { useData } from "@/providers/dataProvider";
+// import { useData } from "@/providers/dataProvider";
 
 
 const BrooklynDemographics = () => {
 
-  const data = useData();
+  // const data = useData();
   // const parsedData: object[] = JSON.parse(data);
-  console.log(data);
+  // console.log(data);
   
 
   return (
--- incident/app/(2005)/brooklyn/layout.tsx
+++ incident/app/(2005)/brooklyn/layout.tsx
@@ -2,23 +2,23 @@
 
 
 //import DataContextProvider from "@/providers/dataProvider"
-import DataProvider from '@/components/clientProvider/DataProvider';
-import { incidentData } from '@/utils/actions';
+// import DataProvider from '@/components/clientProvider/DataProvider';
+// import { incidentData } from '@/utils/actions';
 import { ReactNode } from 'react';
 
 // Can't do it in a secondary layout either
 const BrooklynLayout = ({children}: {children: ReactNode}) => {
 
-  const d = incidentData();
+  //const d = incidentData();
   //const dd: object[] = JSON.parse(d);
   //console.log(dd.slice(400,409));
 
   return (
-    <DataProvider promiseValue={d}>
-      <div className="bg-amber-700">
+    // <DataProvider promiseValue={d}>
+      <div className="bg-blue-200">
         {children}
       </div>
-    </DataProvider>
+    // </DataProvider> 
     
   )
 }
