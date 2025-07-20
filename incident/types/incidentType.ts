9b2686ea917b720fee4673c6f02fa72f47f6216d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-20 10:56:38 -0400

    Created type for shooting incident api data types

---
+++ incident/types/incidentType.ts
@@ -0,0 +1,35 @@
+
+
+type incidentOccurrenceType = {
+  "incident_key"?: string,
+  "occur_date"?: string,
+  "occur_time"?: string,
+  "boro"?: string,
+  "loc_of_occur_desc"?: string,
+  "precinct"?: string,
+  "jurisdiction_code"?: string,
+  "loc_classfctn_desc"?: string,
+  "location_desc"?: string,
+  "statistical_murder_flag"?: string,
+  "perp_age_group"?: string,
+  "perp_sex"?: string,
+  "perp_race"?: string,
+  "vic_age_group"?: string,
+  "vic_sex"?: string,
+  "vic_race"?: string,
+  "x_coord_cd"?: string,
+  "y_coord_cd"?: string,
+  "latitude"?: string,
+  "longitude"?: string,
+  "geocoded_column"?: {
+    "type": string,
+    "coordinates": [number, number]
+  },
+  ":@computed_region_yeji_bk3q"?: string,
+  ":@computed_region_92fq_4b7q"?: string,
+  ":@computed_region_sbqj_enih"?: string,
+  ":@computed_region_efsh_h5xi"?: string,
+  ":@computed_region_f5dn_yrer"?: string  
+};
+
+export default incidentOccurrenceType;
