74641417e83aaf2f445a555bc21452dca59508ff
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-22 18:16:13 -0400

    Wrote use data hooks for the other four non-brooklyn boroughs

--- incident/appHooks/hooks.ts
+++ incident/appHooks/hooks.ts
@@ -9,3 +9,41 @@ export const useBrooklynData = (data: Array<incidentOccurrenceType>): Array<inci
 );
   return filteredData as [incidentOccurrenceType];
 };
+
+export const useBronxData = (data: Array<incidentOccurrenceType>): Array<incidentOccurrenceType> => {
+  const filteredData = data.map(incident => {
+    if(incident.boro === 'BRONX'){
+      return incident;
+    };
+  }
+);
+  return filteredData as [incidentOccurrenceType];
+};
+
+export const useManhattanData = (data: Array<incidentOccurrenceType>): Array<incidentOccurrenceType> => {
+  const filteredData = data.map(incident => {
+    if(incident.boro === 'MANHATTAN'){
+      return incident;
+    };
+  }
+);
+  return filteredData as [incidentOccurrenceType];
+};
+export const useSIData = (data: Array<incidentOccurrenceType>): Array<incidentOccurrenceType> => {
+  const filteredData = data.map(incident => {
+    if(incident.boro === 'STATEN ISLAND'){
+      return incident;
+    };
+  }
+);
+  return filteredData as [incidentOccurrenceType];
+};
+export const useQueensData = (data: Array<incidentOccurrenceType>): Array<incidentOccurrenceType> => {
+  const filteredData = data.map(incident => {
+    if(incident.boro === 'QUEENS'){
+      return incident;
+    };
+  }
+);
+  return filteredData as [incidentOccurrenceType];
+};
