a50ab5c8a8c32ed017cb20ece00ab6b13f26514b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-26 14:55:14 -0400

    Misc

--- incident/app/(2005)/brooklyn/page.tsx
+++ incident/app/(2005)/brooklyn/page.tsx
@@ -7,7 +7,16 @@ import incidentArray from "@/incidentData/incidentArray";
 const BrooklynIncidentsPlots = () => {
   
   const brooklynData = useUniversalData("BROOKLYN", incidentArray);
-  console.log(brooklynData);
+  const bd = brooklynData.slice(0,9);
+
+  const slicedData: Array<[number, number] | undefined> = bd.map((data) => {
+    if(data){
+      return [Number(data.latitude), Number(data.longitude)] as [number, number];
+    };
+  });
+
+  console.log(slicedData);
+  
   
 
   return (
