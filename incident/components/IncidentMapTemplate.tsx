86411a8d5e22ec50d9656091f234121e20cf3537
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-07-27 11:32:18 -0400

    Created a template component to plot incident points with react-leaflet

---
+++ incident/components/IncidentMapTemplate.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
+import { MapContainer, TileLayer, Marker, ZoomControl } from 'react-leaflet';
+//import {FaAccusoft} from "react-icons/fa"
+import { icon } from 'leaflet';
+const iconUrl =
+  'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png';
+const markerIcon = icon({
+  iconUrl: iconUrl,
+  iconSize: [10, 15],
+});
+
+const IncidentMapTemplate = () => {
+  return (
+    <div className="mt-4">
+      <MapContainer center={[40.695051, -73.917793]} zoom={7} scrollWheelZoom={false} zoomControl={false} className='h-[400px] w-[50%] z-0 rounded-lg relative'>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ZoomControl position='bottomright' />
+        {/* {position.map((posit, i) => (
+        <Marker key={i} position={posit} icon={markerIcon}></Marker>
+      ))} */}
+      </MapContainer>
+
+    </div>
+  )
+}
+export default IncidentMapTemplate;
