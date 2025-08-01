a067977174ed98ab7b2a1131e32b9f392a02f695
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-01 18:14:42 -0400

    Added staten island plots to its homepage

--- incident/app/(2005)/si/page.tsx
+++ incident/app/(2005)/si/page.tsx
@@ -1,14 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+///* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
 import { useUniversalData } from "@/appHooks/hooks";
 import incidentArray from "@/incidentData/incidentArray";
+import dynamic from "next/dynamic";
 
+const MapProperty = dynamic(
+  () => import('@/components/IncidentMapTemplate'),
+  { ssr: false }
+);
 
 const StatenIslandIncidentsPlots = () => {
 
   const statenIslandData = useUniversalData("STATEN ISLAND", incidentArray);
+  //const bdTemporary = brooklynData.slice(0,9);
+
+  const slicedData: Array<[number, number]> = statenIslandData.map((data) => {
+    return [Number(data.latitude), Number(data.longitude)] as [number, number];
+  });
 
   return (
-    <div>StatenIslandIncidentsPlots</div>
+    <>
+      <MapProperty data={slicedData} />
+    </>
   )
 }
 export default StatenIslandIncidentsPlots;
