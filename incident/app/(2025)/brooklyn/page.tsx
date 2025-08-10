5fee7c7b1f99e4a36f780e5d5b9d117ca1804b90
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-10 14:54:47 -0400

    Returned data properly configured to new type

--- incident/app/(2025)/brooklyn/page.tsx
+++ incident/app/(2025)/brooklyn/page.tsx
@@ -11,11 +11,11 @@ const MapProperty = dynamic(
 );
 
 type DataReturn = {
-  coordinates: [number, number],
-  murderOutcome: string,
-  ageGroup: string,
-  race: string,
-  sex: string
+  coordinates?: [number, number],
+  murderOutcome?: string,
+  ageGroup?: string,
+  race?: string,
+  sex?: string
 }
 
 const BrooklynIncidentsPlots = () => {
@@ -23,8 +23,15 @@ const BrooklynIncidentsPlots = () => {
   const brooklynData = useUniversalData("BROOKLYN", incidentArray);
   //const bdTemporary = brooklynData.slice(0,9);
 
-  const slicedData: Array<[number, number]> = brooklynData.map((data) => {
-    return [Number(data.latitude), Number(data.longitude)] as [number, number];
+  const slicedData: Array<DataReturn> = brooklynData.map((data) => {
+    //return [Number(data.latitude), Number(data.longitude)] as [number, number];
+    return {
+      coordinates: [Number(data.latitude), Number(data.longitude)] as [number, number],
+      murderOutcome: data.statistical_murder_flag,
+      ageGroup: data.vic_age_group,
+      race: data.vic_race,
+      sex: data.vic_sex,
+    }
   });
   
   return (
