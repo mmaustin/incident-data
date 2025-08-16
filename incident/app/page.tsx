78d1f939cf4668afa94609af4d166dbfc2c5aa95
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-16 17:30:22 -0400

    Added a return type to temporary mapping function

--- incident/app/page.tsx
+++ incident/app/page.tsx
@@ -5,21 +5,21 @@ import incidentArray from "@/incidentData/incidentArray";
 
 export default async function Home() {
 
-  const mappedData = new Map();
+  const mappedData = new Map<string, number>();
   
-  const mappedDataMaker = () => {
+  const mappedDataMaker = (): Map<string, number> => {
     
     incidentArray.map(d => {
       if(mappedData.has(d.vic_age_group)){
-        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group)+1);
+        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group) as number +1);
       } else {
         mappedData.set(d.vic_age_group, 1);
       }
     });
-    console.log(mappedData);
+    return mappedData;
     
   };
-  mappedDataMaker()
+  console.log(mappedDataMaker());
   
   return (
     <div className="bg-amber-200 uppercase h-screen flex flex-col justify-center items-center">
