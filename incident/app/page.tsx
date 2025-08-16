f425f12f5d2159726dc5979c7c6299d62b2014a4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-16 17:12:47 -0400

    Mapped over data to log the new Map created with victim age ranges

--- incident/app/page.tsx
+++ incident/app/page.tsx
@@ -1,19 +1,43 @@
-/* eslint-disable react/no-children-prop */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import incidentArray from "@/incidentData/incidentArray";
+
 // import DataProvider from "@/components/clientProvider/DataProvider";
-import { incidentData } from "@/utils/actions";
+//import { incidentData } from "@/utils/actions";
 // import { ReactNode } from "react";
 
 export default async function Home() {
+//   const mapIt = () => {
+//     data.map(d => {
+//         if(holder.has(d.vic_age_group)){
+//             holder.set(d.vic_age_group, holder.get(d.vic_age_group)+1)
+//         } else {
+//             holder.set(d.vic_age_group, 1);
+//         }
+//     });
+//     console.log(holder)
+// };
 
-  // const data: Promise<string> = incidentData();
-  // console.log(typeof data);
+  const mappedData = new Map();
+  
+  const mappedDataMaker = () => {
+    
+    incidentArray.map(d => {
+      if(mappedData.has(d.vic_age_group)){
+        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group)+1);
+      } else {
+        mappedData.set(d.vic_age_group, 1);
+      }
+    });
+    console.log(mappedData);
+    
+  };
+  mappedDataMaker()
   
   return (
-    <div className="bg-amber-200 uppercase h-screen">
-     up and running???
-     {/* <DataProvider promiseValue={data} children={undefined}/> */}
+    <div className="bg-amber-200 uppercase h-screen flex flex-col justify-center items-center">
+     GRIM DATA REPORT
     </div>
   );
 }
