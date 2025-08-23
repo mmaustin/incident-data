129039db6db202c2c621c4ec9cd9ef09f74141dd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-23 16:03:02 -0400

    Misc

--- incident/app/page.tsx
+++ incident/app/page.tsx
@@ -1,7 +1,9 @@
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
 
 import incidentArray from "@/incidentData/incidentArray";
+import { Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 
 // Map(6) {
 //   '45-64' => 47,
@@ -12,42 +14,55 @@ import incidentArray from "@/incidentData/incidentArray";
 //   'UNKNOWN' => 1
 // }
 
-type DataIncidentType = 
-  {name: string, incidents: number}
+type DataIncidentType =
+  { name: string, incidents: number }
 
 
 type ChartDataIncidentType = Array<DataIncidentType>;
 
-export default async function Home() {
+export default function Home() {
 
   const mappedData = new Map<string, number>();
 
   const arr: DataIncidentType[] = [];
-  
-  const mappedDataMaker = (): Map<string, number> => {
-    
+
+  const mappedDataMaker = (): void => {
+
     incidentArray.map(d => {
-      if(mappedData.has(d.vic_age_group)){
-        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group) as number +1);
+      if (mappedData.has(d.vic_age_group)) {
+        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group) as number + 1);
       } else {
         mappedData.set(d.vic_age_group, 1);
       }
     });
-    for(const [key, value] of mappedData){
-      arr.push({name: key, incidents: value});
-    }
-    
-    return mappedData;
-    
+    for (const [key, value] of mappedData) {
+      arr.push({ name: key, incidents: value });
+    };
+
   };
   mappedDataMaker();
 
-  console.log(arr);
-  
-  
+  //console.log(arr);
+
+
   return (
-    <div className="bg-amber-200 uppercase h-screen flex flex-col justify-center items-center">
-     GRIM DATA REPORT
+    <div className="h-[500px] w-full flex flex-col justify-center items-center border-2 border-amber-300">
+      <p>what the what</p>
+      <ResponsiveContainer width='100%' height={300} className='mx-4 border-2'>
+        <PieChart >
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={arr}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   );
 }
