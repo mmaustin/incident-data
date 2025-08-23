cf0c3209294508fb70d6d0e1886e652cbbc07afd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-23 16:22:44 -0400

    Fixed error with recharts chart not rendering

--- incident/app/page.tsx
+++ incident/app/page.tsx
@@ -14,6 +14,15 @@ import { Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 //   'UNKNOWN' => 1
 // }
 
+const data01 = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 },
+  { name: 'Group D', value: 200 },
+  { name: 'Group E', value: 278 },
+  { name: 'Group F', value: 189 },
+];
+
 type DataIncidentType =
   { name: string, incidents: number }
 
@@ -51,7 +60,7 @@ export default function Home() {
       <ResponsiveContainer width='100%' height={300} className='mx-4 border-2'>
         <PieChart >
           <Pie
-            dataKey="value"
+            dataKey="incidents"
             isAnimationActive={false}
             data={arr}
             cx="50%"
---
+++ incident/components/display/DemoChartDisplay.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+import incidentArray from '@/incidentData/incidentArray';
+import { Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
+
+const DemoChartDisplay = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+export default DemoChartDisplay
