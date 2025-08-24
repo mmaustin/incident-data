fa23478d840a0a1265b288be4d445d49f0c525e5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-24 14:54:35 -0400

    Removed commented code from temporary homepage

--- incident/app/page.tsx
+++ incident/app/page.tsx
@@ -1,60 +1,15 @@
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
+import { useRechartsDataMaker, useUniversalData } from "@/appHooks/hooks";
 import incidentArray from "@/incidentData/incidentArray";
 import { Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
 
 
-// Map(6) {
-//   '45-64' => 47,
-//   '18-24' => 109,
-//   '25-44' => 220,
-//   '<18' => 58,
-//   '65+' => 4,
-//   'UNKNOWN' => 1
-// }
+export default function Home() {
 
-
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
-];
-
-type DataIncidentType = { name: string, incidents: number };
-
-
-type ChartDataIncidentType = Array<DataIncidentType>;
-
-export default function Home() {  
-
-  const mappedData = new Map<string, number>();
-
-  const arr: DataIncidentType[] = [];
-  
-  const mappedDataMaker = (): DataIncidentType[] => {
-    
-    incidentArray.map(d => {
-      if (mappedData.has(d.vic_age_group)) {
-        mappedData.set(d.vic_age_group, mappedData.get(d.vic_age_group) as number + 1);
-      } else {
-        mappedData.set(d.vic_age_group, 1);
-      }
-    });
-    for (const [key, value] of mappedData) {
-      arr.push({ name: key, incidents: value });
-    };
-
-    return arr;
-
-  };
-  mappedDataMaker();
-
-  //console.log(arr);
+  const boroughData = useUniversalData("BROOKLYN", incidentArray);
+  const arr = useRechartsDataMaker(boroughData);
 
 
   return (
