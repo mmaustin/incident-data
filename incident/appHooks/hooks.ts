ebe5b10f251a6272c153c1485a15c6f3523e2728
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-08-30 14:33:10 -0400

    Created a function to map and graph murder outcomes

--- incident/appHooks/hooks.ts
+++ incident/appHooks/hooks.ts
@@ -76,3 +76,24 @@ export const useRechartsDataMaker = (boroughData: Array<IncidentOccurrenceType>)
 
   return arr;
 };
+
+export const useRechartsDataMakerMurder = (boroughData: Array<IncidentOccurrenceType>): Array<DataIncidentType> => {
+
+  const mappedData = new Map<string, number>();
+
+  const arr: DataIncidentType[] = [];
+
+  boroughData.map(dataSet => {
+    if (mappedData.has(dataSet.statistical_murder_flag)) {
+      mappedData.set(dataSet.statistical_murder_flag, mappedData.get(dataSet.statistical_murder_flag) as number + 1);
+    } else {
+      mappedData.set(dataSet.statistical_murder_flag, 1);
+    }
+  });
+
+  for (const [key, value] of mappedData) {
+    arr.push({ name: key, incidents: value });
+  };
+
+  return arr;
+};
