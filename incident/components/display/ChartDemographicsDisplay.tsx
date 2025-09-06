8ac3693a0f3d060b0f8b8c0047cbdfef6dbb691e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-06 14:09:18 -0400

    Completed demographics display chart's initial stage

--- incident/components/display/ChartDemographicsDisplay.tsx
+++ incident/components/display/ChartDemographicsDisplay.tsx
@@ -2,6 +2,7 @@
 import { useRechartsDataMakerDemographics, useUniversalData } from "@/appHooks/hooks";
 import incidentArray from "@/incidentData/incidentArray";
 import { boroughInput } from "@/types/boroughType";
+import { Pie, PieChart, ResponsiveContainer } from 'recharts';
 
 const ChartDemographicsDisplay = ({borough}: {borough: boroughInput}) => {
 
@@ -9,9 +10,12 @@ const ChartDemographicsDisplay = ({borough}: {borough: boroughInput}) => {
   const demographicsArray = useRechartsDataMakerDemographics(boroughData);
 
   return (
-    <div>
-      {demographicsArray.length}
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+    <PieChart width={400} height={400}>
+      <Pie data={demographicsArray[0]} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+      <Pie data={demographicsArray[1]} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+    </PieChart>
+  </ResponsiveContainer>
   )
 }
 export default ChartDemographicsDisplay
