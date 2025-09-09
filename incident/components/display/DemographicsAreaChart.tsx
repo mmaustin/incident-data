669ef676ed3b66b3fae04f6e689a9e3cc7c5a7c1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-09 17:57:19 -0400

    Imported chart and function components

--- incident/components/display/DemographicsAreaChart.tsx
+++ incident/components/display/DemographicsAreaChart.tsx
@@ -1,6 +1,14 @@
 'use client';
+import incidentArray from '@/incidentData/incidentArray';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { boroughInput } from '@/types/boroughType';
+import { useRechartsDataMakerDemographics, useUniversalData } from "@/appHooks/hooks";
 
-const DemographicsAreaChart = () => {
+const DemographicsAreaChart = ({borough}: {borough: boroughInput}) => {
+
+  const boroughData = useUniversalData(borough, incidentArray);
+  const demographicsArray = useRechartsDataMakerDemographics(boroughData);
+
   return (
     <div>DemographicsAreaChart</div>
   )
