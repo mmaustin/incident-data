021a29c0f21c2731ef0c3aa77c378ced529709e7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-13 12:42:46 -0400

    Added text and minimal styling to the two demographics charts

--- incident/components/display/DemographicsAreaChart.tsx
+++ incident/components/display/DemographicsAreaChart.tsx
@@ -12,7 +12,8 @@ const DemographicsAreaChart = ({ borough }: { borough: boroughInput }) => {
   return (
     <div className="h-full flex flex-wrap justify-center items-center mt-16">
 
-      <div className="h-[210px] w-[45%] ">
+      <div className="h-[210px] w-[45%] flex flex-col justify-center items-center ">
+        <p className="font-bold ">Victim Demographics</p>
         <ResponsiveContainer width="100%" height="100%">
           <AreaChart
             width={500}
@@ -34,26 +35,27 @@ const DemographicsAreaChart = ({ borough }: { borough: boroughInput }) => {
         </ResponsiveContainer>
       </div>
 
-      <div className="h-[210px] w-[45%] ">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data[1]}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="incidents" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
-        </ResponsiveContainer>
+      <div className="h-[210px] w-[45%] flex flex-col justify-center items-center">
+      <p className="font-bold ">Perpetrator Demographics</p>
+      <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data[1]}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="incidents" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
       </div>
 
     </div>
