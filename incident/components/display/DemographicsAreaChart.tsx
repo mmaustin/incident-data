9791943e455f908c6465795a6429302dcda0fc7d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-09-13 12:14:13 -0400

    Added perp data to the demographics area chart

--- incident/components/display/DemographicsAreaChart.tsx
+++ incident/components/display/DemographicsAreaChart.tsx
@@ -10,27 +10,54 @@ const DemographicsAreaChart = ({ borough }: { borough: boroughInput }) => {
   const data = useRechartsDataMakerDemographics(boroughData);
 
   return (
-    <div className="h-[310px] w-[95%] flex flex-col justify-center items-center">
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          width={500}
-          height={400}
-          data={data[0]}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="incidents" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
-      </ResponsiveContainer>
+    <div className="h-full">
+
+      <div className="h-[310px] w-[95%] flex flex-col justify-center items-center">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data[0]}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="incidents" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+
+      <div className="h-[310px] w-[95%] flex flex-col justify-center items-center">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data[1]}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area type="monotone" dataKey="incidents" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+
     </div>
+
   )
 }
 export default DemographicsAreaChart;
