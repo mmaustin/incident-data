aaca607389d28855aee4f89aedafb6e16770052e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-10-23 20:34:18 -0400

    Installed react-speakup

--- incident/app/(voice-text)/ttv/page.tsx
+++ incident/app/(voice-text)/ttv/page.tsx
@@ -1,10 +1,34 @@
-"use client"
+"use client";
+
+import {useTextToVoice} from "react-speakup";
 
 
+const TextToVoice = () => {
 
-const page = () => {
+  const { speak, pause, resume, ref, setVoice, voices } = useTextToVoice<HTMLDivElement>();
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <button onClick={speak}>Speak</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <select
+        onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+          setVoice(event.target.value)
+        }
+      >
+        {voices.map((voice) => (
+          <option key={voice}>{voice}</option>
+        ))}
+      </select>
+      <div ref={ref}>
+        <h1>It&apos;s not important which HTML tag your text is within.</h1>
+        <div>
+          Or <p>how many levels it is nested.</p>
+        </div>
+      </div>
+    </div>
+  );
+
 }
-export default page
+export default TextToVoice;
